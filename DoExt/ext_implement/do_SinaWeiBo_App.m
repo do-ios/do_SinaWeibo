761141c5db3011dcdaf306855c01c4a4d5adc1fe
yangzhen <1784054000@qq.com>
2015-05-09 15:53:36 +0800

    提交SinaWeibo组件源代码

---
+++ DoExt/ext_implement/do_SinaWeiBo_App.m
@@ -0,0 +1,44 @@
+//
+//  do_SinaWeiBo_App.m
+//  DoExt_SM
+//
+//  Created by 刘吟 on 15/4/9.
+//  Copyright (c) 2015年 DoExt. All rights reserved.
+//
+
+#import "do_SinaWeiBo_App.h"
+#import "WeiboSDK.h"
+#import "doServiceContainer.h"
+#import "doIModuleExtManage.h"
+
+
+@implementation do_SinaWeiBo_App
+@synthesize ThridPartyID;
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+//    NSString *sinaKey = [[doServiceContainer Instance].ModuleExtManage GetThirdAppKey:@"do_SinaWeiBo.plist" :@"EASEMOB_APPKEY"];
+//    [WeiboSDK enableDebugMode:YES];
+//    [WeiboSDK registerApp:@"1226508063"];
+    return YES;
+}
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+    
+}
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+    
+}
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+    
+}
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+    
+}
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+    
+}
+@end
